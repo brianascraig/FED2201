216e493f7f28fa317d32eb4e51b2356c410d144b
Briana Craig <brianascraig@gmail.com>
2022-01-17 10:06:25 -0500

    added audio and updated JS

--- Assignment/00 - About Me (HTML)/script.js
+++ Assignment/00 - About Me (HTML)/script.js
@@ -1,5 +1,12 @@
 function displayWelcome(){
-    
+    let welcomeForm = document.querySelector(".welcomeForm");
+    welcomeForm.addEventListener("submit", handleSubmit) 
+}
+
+function handleSubmit(event){
+    event.preventDefault();
     alert("Welcome!");
     return;
 }
+
+displayWelcome();
