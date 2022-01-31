f4765e5f55d9e0176318b2ec823a01288bb38a81
Briana Craig <brianascraig@gmail.com>
2022-01-30 21:45:29 -0500

    updated js

---
+++ Assignment/06 - CRUD App/data.js
@@ -0,0 +1,8 @@
+let events = [
+    {
+        "id" : 01,
+        "name" : "Sleeping",
+        "time" : "12 am",
+        "detail" : "Sleeping til 6am"
+    }
+]
--- Assignment/06 - CRUD App/index.js
+++ Assignment/06 - CRUD App/index.js
@@ -5,29 +5,48 @@ function handleApp(){
     let deleteBtn = document.querySelector(".delete");
     let viewBtnId = viewBtn.parentNode.id;
     let createBtnId = createBtn.parentNode.id;
+    let editBtnId = editBtn.parentNode.id;
+    let deleteBtnId = deleteBtn.parentNode.id;
     createBtn.addEventListener("click", createEvent(createBtnId));
     viewBtn.addEventListener("click", readEvent(viewBtnId));
-    editBtn.addEventListener("click", updateEvent);
-    deleteBtn.addEventListener("click", deleteEvent);
-}
-
-function getEventId(){
-
+    editBtn.addEventListener("click", updateEvent(editBtnId));
+    deleteBtn.addEventListener("click", deleteEvent(deleteBtnId));
 }
 
 function createEvent(id){
-    
+    console.log("id = " + id);
+    let form = document.querySelector(".formSection");
+    let agenda = document.querySelector(".agendaSection");
+    agenda.toggleAttribute("hidden");
+    form.toggleAttribute("hidden");
+    form.addEventListener("submit", ()=>{
+       var eventName = document.getElementById("name").value;
+       var time = document.getElementById("time").value;
+       var detail = document.getElementById("detail").value; 
+    });
+    events.push({
+        "id" : id,
+        "name" : eventName,
+        "time" : time,
+        "detail" : detail
+    });
+    loadNewEvent(id);
+}
+
+function loadNewEvent(id){
+    let eventSlot = document.getElementById(id);
+    let eventSlotTime = eventSlot.childNodes.hasAttribute("time");
 }
 
 function readEvent(id){
+    console.log("id = " + id);
+}
+
+function updateEvent(id){
 
 }
 
-function updateEvent(){
-
-}
-
-function deleteEvent(){
+function deleteEvent(id){
 
 }
 
