6817a89ab04c752ab91dd6f9199ad25a0afdefb9
Briana Craig <brianascraig@gmail.com>
2022-02-11 10:22:45 -0500

    updated dashboard

--- Assignment/04 - Twitter Clone (HTML & CSS)/js/dashboard.js
+++ Assignment/04 - Twitter Clone (HTML & CSS)/js/dashboard.js
@@ -3,4 +3,50 @@ function loadUserDashboard(userKey, userId){
     console.log("=====INSIDE DASHBOARD====="); 
     console.log("userKey" + userKey);
     console.log("user Id: " +userId);
+    displayGrapevines(userKey);
+    displayTrends();
+    displayUserInfo();
 }
+
+function displayGrapevines(){
+    let grapevines = [];
+    let grapevinesSection = $(".grapevinesSection");
+    $.get(`${baseUrl}/grapevines${jsonExt}`, function(data, status){
+      console.log("success")
+      }).then((data)=>{ 
+        for (var key in data) {
+          grapevines.push(key);
+      }
+        console.log(data);
+        console.log("grapevines arr: " + grapevines); 
+        // console.log(`url= ${baseUrl}/users/${users[0]}${jsonExt}`);
+        for (let i = 0; i < grapevines.length; i++){
+          console.log("inside for displayGrapevines");
+          $.get(`${baseUrl}/grapevines/${grapevines[i]}${jsonExt}`
+          , function(data, status){
+          console.log("success");
+          }).then((data)=>{
+            console.log("then id" + id)
+            let userName = data.userName;
+            let content = data.content; 
+            grapevinesSection.append(`
+                <article>
+                    <div>
+                        <img
+                            src="../images/avatar.png"
+                            alt="Profile photo avatar"
+                            class="userAvatar"
+                        />
+                        <span>${userName}</span>
+                    </div>
+                    <div>
+                        <p>${content}</p>
+                    </div>
+                </article>`)
+        console.log("data.username: " + data.userName);
+        console.log("data content" + data.content);
+      })
+    }
+    }) 
+                
+  } 
