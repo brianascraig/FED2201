55bbe659e63ef18abfd273b98284606ad07c47a4
Briana Craig <brianascraig@gmail.com>
2022-02-21 09:56:09 -0500

    updated HTML, CSS and JS

--- Assignment/09 - Coding Practice 3/index.js
+++ Assignment/09 - Coding Practice 3/index.js
@@ -22,5 +22,16 @@ function getDaysFromToday(){
     let date = new Date();
     let today = date.getTime();
     let futureDate = new Date("01/01/2030");
-    
+    let timeElapsed = futureDate.getTime() - today;
+    let milliseconds = 1000;
+    let minutes = 60;
+    let seconds = 60;
+    let hours = 24
+    let daysElapsed = timeElapsed/ (milliseconds * seconds * minutes * hours);
+    displayDaysFromToday(daysElapsed);
+}
+
+function displayDaysFromToday(daysElapsed){
+    let daysReveal = $(".daysReveal");
+    daysReveal.append(`${daysElapsed}`);
 }
